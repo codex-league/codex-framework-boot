c77be3e23163f7cb9f2a5198739e3d3133bf8a82
许西 <xuxi@xuxideMacBook-Pro.local>
2023-04-26 10:13:54 +0800

    增加user center的资源访问限制

---
+++ codex-server-user-center/src/main/java/pub/codex/user/config/ResourceServerConfig.java
@@ -0,0 +1,50 @@
+package pub.codex.user.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * 资源服务器配置
+ *
+ */
+@Configuration
+@EnableWebSecurity
+public class ResourceServerConfig {
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/codex/**").permitAll()
+                        .requestMatchers("/error").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .csrf().disable()
+                .oauth2ResourceServer()
+                .jwt()
+                .decoder(jwtDecoder());
+        return http.build();
+    }
+
+
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+
+        ServiceInstance serviceInstance = loadBalancerClient.choose("codex-server-oauth-center");
+        //// TODO: 2023/4/26 待优化
+        return NimbusJwtDecoder.withJwkSetUri(serviceInstance.getUri().toString() + "/oauth2/jwks").build();
+    }
+
+}
